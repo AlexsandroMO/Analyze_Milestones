0b3d57dff2fb234ecc3d35481d2cdff0ae6fed71
Alexsandro Monteiro de Oliveira <myhome@MacBook-Air-de-Alexsandro.local>
2021-10-29 23:06:26 -0300

    start project

---
+++ Milestones_Control/urls.py
@@ -0,0 +1,12 @@
+"""Milestones_Control URL Configuration
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('', include('task.urls')),
+    path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+
+]
